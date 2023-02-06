4faab1836dbd2b3570494a24247ba65e00caa02a
Omixxx <xxcortex99@gmail.com>
2023-02-06 16:48:20 +0100

    introduced guard dinamic type checker

--- backend/src/controllers/Search.ts
+++ backend/src/controllers/Search.ts
@@ -1,10 +1,14 @@
 import exists from "../utils/exists";
-import { SearchRequest } from "../../../types/types";
+import { isSearchRequest, SearchRequest } from "../../../types/types";
 import assert from "assert";
 import { Request, Response } from "express";
 
 export async function search(req: Request, res: Response) {
   assert(exists(req.body), "Request body is undefined");
+  assert(
+    isSearchRequest(req.body),
+    "Request body is not of type SearchRequest"
+  );
 
   const searchRequest: SearchRequest = req.body;
 
